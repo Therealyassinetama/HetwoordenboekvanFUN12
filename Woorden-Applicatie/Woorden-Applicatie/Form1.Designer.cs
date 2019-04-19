ff352173c2480bc6ccc0e9a019e27345abb73aba
Therealyassinetama <49792918+Therealyassinetama@users.noreply.github.com>
2019-04-19 23:25:22 +0200

    Add files via upload

---
+++ Woorden-Applicatie/Woorden-Applicatie/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Woorden_Applicatie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CHECK CHECK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "DELETE WORD";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "DELETE ALL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "ADD WORD";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "betekenis";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(122, 180);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "CHANGE WORD AND MEANING";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "BETEKENIS";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(122, 154);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(236, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 186);
+            this.listBox1.TabIndex = 9;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(363, 13);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 186);
+            this.listBox2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 205);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Woorden-Applicatie/Woorden-Applicatie/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Woorden_Applicatie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<string> woorden = new List<string>();
+        public List<string> betekenis = new List<string>();
+
+        public bool NieuwWoord(string woord)
+        {
+            if (woorden.Contains(woord))
+            {
+                if(woord == "Fun12 is echt een \"leuk\" vak")
+                {
+                    Form2 dank = new Form2();
+                    dank.Show();
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string betekeniszoeker(string woord)
+        {
+            int index = woorden.IndexOf(woord);
+            return betekenis[index];
+        }
+
+        public void updater(string woord, string betekenisx, string newwoord)
+        {
+            int index = woorden.IndexOf(woord);
+            betekenis[index] = betekenisx;
+            woorden[index] = newwoord;
+        }
+
+        public void listboxfiller()
+        {
+            foreach (var gay in woorden)
+            {
+                listBox1.Items.Add(gay);
+            }
+            foreach (var gay2 in betekenis)
+            {
+                listBox2.Items.Add(gay2);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (NieuwWoord(textBox1.Text) == true)
+                {
+                    MessageBox.Show("YEAH IT DOES YES ");
+                }
+                else
+                {
+                    if (textBox1.Text == "Fun12 is echt een \"leuk\" vak")
+                    {
+                        MessageBox.Show("voeg het toe dit is een coole easteregg");
+                    }
+                    else
+                    {
+                        MessageBox.Show("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+                    }
+                }
+                label1.Text = betekeniszoeker(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Woordenboek is leeg");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int index = woorden.IndexOf(textBox1.Text);
+            woorden.Remove(textBox1.Text);
+            betekenis.Remove(textBox2.Text);
+            listBox1.Items.Remove(index);
+            listBox2.Items.Remove(index);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            woorden.Clear();
+            betekenis.Clear();
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            woorden.Add(textBox1.Text);
+            if(textBox2.Text == null)
+            {
+                betekenis.Add("Geen betekenis opgegeven");
+            }
+            else
+            {
+
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                betekenis.Add(textBox2.Text);
+                listboxfiller();
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            updater(textBox1.Text, textBox2.Text, textBox3.Text);
+            listboxfiller();
+        }
+    }
+}
